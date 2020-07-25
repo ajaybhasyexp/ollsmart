e6f062a7e825042cd473318761710ba0640db702
Ajay Bhasy <abhasy@acr.org>
2020-07-25 11:08:58 +0530

    Connection string issue fix

--- src/Services/ConfigurationService.cs
+++ src/Services/ConfigurationService.cs
@@ -8,36 +8,30 @@ namespace OllsMart.Services
     {
         private IConfiguration _configuration = null;
         private IWebHostEnvironment _env = null;
-        public ConfigurationService(IConfiguration configuration, 
-        IWebHostEnvironment env) {
+        public ConfigurationService(IConfiguration configuration,
+        IWebHostEnvironment env)
+        {
             _configuration = configuration;
             _env = env;
         }
 
-        public string GetConnectionString() {
-            var dbhost = _configuration.GetValue<string>("CWL_DB_HOST", "localhost");
-            var dbname = _configuration.GetValue<string>("CWL_DB_NAME", "CWL.VirtualCare");
-            var dbuser = _configuration.GetValue<string>("CWL_DB_USER", null);
-            var dbpassword = _configuration.GetValue<string>("CWL_DB_PASSWORD", null);
-            var dbport = _configuration.GetValue<int>("CWL_DB_PORT", 0);
-
-            if (string.IsNullOrEmpty(dbuser)) {
-                return GetDefaultConnectionString();
-            }
-
-            if (string.IsNullOrEmpty(dbpassword)) {
-                return GetDefaultConnectionString();
-            }
-
-            return $"server={dbhost};database={dbname};user={dbuser};password={dbpassword}";
+        public string GetConnectionString()
+        {
+            return GetDefaultConnectionString();
         }
 
-        public string GetDefaultConnectionString() {
-            if (_env.IsDevelopment()) {
+        public string GetDefaultConnectionString()
+        {
+            if (_env.IsDevelopment())
+            {
                 return _configuration.GetConnectionString("Local");
-            } else if (_env.IsStaging()) {
+            }
+            else if (_env.IsStaging())
+            {
                 return _configuration.GetConnectionString("Staging");
-            } else {
+            }
+            else
+            {
                 return _configuration.GetConnectionString("Prodcution");
             }
         }
